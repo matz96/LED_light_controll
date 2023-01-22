ba4ef59eb039c844c284787b8d6b947182f34360
Matthias Bhend <bhend-matz@bluewin.ch>
2023-01-22 11:45:45 +0100

    funktioniert nömme

---
+++ matz_gewurstel/main.js
@@ -0,0 +1,48 @@
+const colorjoe = require("./colorjoe");
+
+/*const get_state = async() => {
+    const state = await fetch("http://localhost/");
+    const data = await state.json();
+    const status = data.status;
+    const Red = data.Red;
+    const Blue = data.Blue;
+    const Green = data.Green;
+    const Alpha = data.Alpha;
+    const joe = colorjoe.rgb('color-picker', (Red,Blue,Green), ['currentColor','alpha',['fields', {space: 'RGB', limit: 255, fix: 0}],'hex'])
+
+}*/
+
+
+
+
+
+window.onload = () => {
+    //get_state();
+
+
+
+   /* const on_but = document.getElementById("ON");
+    on_but.addEventListener("click", function () {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", "http://localhost/", true);
+        xhr.send();
+    });
+
+    const off_but = document.getElementById("OFF");
+    off_but.addEventListener("click", function () {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", "http://localhost/", true);
+        xhr.send();
+    });
+
+    const toggle = document.getElementById("Toggle");
+    toggle.addEventListener("click", function () {
+        const xhr = new XMLHttpRequest();
+        const toggle = status;
+        const url =
+
+        xhr.open("GET", "http://localhost/", true);
+        xhr.send();
+    });*/
+
+}
