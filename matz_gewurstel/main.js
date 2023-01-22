4b65a4e48861c9ca7e9d550dc94bc739149c46aa
Matthias Bhend <bhend-matz@bluewin.ch>
2023-01-22 13:03:50 +0100

    current

--- matz_gewurstel/main.js
+++ matz_gewurstel/main.js
@@ -1,7 +1,17 @@
-const colorjoe = require("./colorjoe");
+import colorjoe from "./colorjoe";
+import axios from "https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js";
 
-/*const get_state = async() => {
-    const state = await fetch("http://localhost/");
+const axiosGET = axios.create({
+
+    baseURL: "http://localhost:3000/api",
+  
+    method: 'get'
+  
+  })
+
+
+const get_state = async() => {
+    const state = await axiosGET;
     const data = await state.json();
     const status = data.status;
     const Red = data.Red;
@@ -9,29 +19,26 @@ const colorjoe = require("./colorjoe");
     const Green = data.Green;
     const Alpha = data.Alpha;
     const joe = colorjoe.rgb('color-picker', (Red,Blue,Green), ['currentColor','alpha',['fields', {space: 'RGB', limit: 255, fix: 0}],'hex'])
-
-}*/
-
+    return data;
+}
 
 
 
 
 window.onload = () => {
-    //get_state();
+    data = get_state();
+    const status = data.status;
 
 
-
-   /* const on_but = document.getElementById("ON");
+    const on_but = document.getElementById("ON");
     on_but.addEventListener("click", function () {
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://localhost/", true);
-        xhr.send();
+       axiosGET.get()
     });
 
     const off_but = document.getElementById("OFF");
     off_but.addEventListener("click", function () {
         const xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://localhost/", true);
+        xhr.open("GET", "localhost:3000/api", true);
         xhr.send();
     });
 
@@ -43,6 +50,6 @@ window.onload = () => {
 
         xhr.open("GET", "http://localhost/", true);
         xhr.send();
-    });*/
+    });
 
 }
