8652ada91fc33408e12aa85c9cc5e6bfb430b515
Cyril <cyril.odermatt@crealsolutions.ch>
2023-01-22 23:59:18 +0100

    last comments

--- client/main.js
+++ client/main.js
@@ -23,27 +23,33 @@ window.onload = async () => {
     const joe = colorjoe.rgb('color-picker', 'red', ['currentColor', 'alpha', ['fields', { space: 'RGB', limit: 255, fix: 0 }], 'hex']) 
     
     const e_body = document.querySelector("body")
+    
     //sets the background to the color, that the curser is at in the 2D-Plane or on the sliders
     joe.on("change", color => e_body.style.backgroundColor = color.cssa());
-    //converses R,G,B values to a Hex values
+    
+    //converts R,G,B values to a Hex values to set initial condition of color picker.
     colorString = `#${data.red.toString(16).padStart(2, '0')}${data.green.toString(16).padStart(2, '0')}${data.blue.toString(16).padStart(2, '0')}`
     
     // set the RGB and the alpha value
     joe.set(colorString)
     joe.setAlpha(data.gain/100.)
 
-    
+    // when color is changed and mouse let go or tab when entering number in fields this event is triggered
     joe.on("done", color => {
+
+        //get color values from colorjoe
         const red = Math.floor(255*color.red())
         const green = Math.floor(255*color.green())
         const blue = Math.floor(255*color.blue())
         
+        //get alpha value from color joe, convert it to 0..100 and send to api
         setIntensity(Math.floor(joe.getAlpha()*100))
         setColor("red",red)
         setColor("green",green)
         setColor("blue",blue)
     })
 
+    //Button listener and api call for On Button
     const on_but = document.getElementById("ON");
     on_but.addEventListener("click", function () {
        axiosGET.get("?turn=on")
@@ -63,10 +69,12 @@ window.onload = async () => {
 
 }
 
+//function to set color via api
 async function setColor(color, value){
     const response = await axiosGET.get(`?${color}=${value}`)
 }
 
+//function to set intensity via api
 async function setIntensity(value){
     const response = await axiosGET.get(`?intensity=${value}`)
 }
